afed26c8b69d8b90891766d77f009be43818f36b
Utkarsh tiwari <utkarshkumartiwari999@gmail.com>
2026-09-17 01:34:42 +0530

    rewrote the collector code for practice and concluded phase 1

--- src/db_manager.py
+++ src/db_manager.py
@@ -36,31 +36,31 @@ if __name__ == "__main__":
     if not connect:
         print("Pipeline aborted: Database offline.")
         exit()
-        
+
     cursor = connect.cursor()
-    
+
     file_paths = glob.glob("data/raw_*.json")
-    
+
     for file_path in file_paths:
         filename = os.path.basename(file_path)
-        zone_name = filename.replace("raw_", "").replace(".json", "")
-        
+        zone_name = filename.replace("raw", "").replace(".json", "")
+
         if zone_name == "sample":
             continue
 
         print(f"Processing {zone_name}...")
-        
+
         with open(file_path, "r") as f:
             data = json.load(f)
-            
+
         loc_id = insert_location(cursor, zone_name, data['latitude'], data['longitude'])
-        
+
         hourly = data['hourly']
         times = hourly['time']
         temperatures = hourly['temperature_2m']
         precipitations = hourly['precipitation']
         wind_speeds = hourly['wind_speed_10m']
-        
+
         query = """
             INSERT INTO environmental_metrics 
             (location_id, recorded_timestamp, temperature_c, precipitation_mm, wind_speed_kmh) 
@@ -68,11 +68,10 @@ if __name__ == "__main__":
         """
         for t, temp, precip, wind in zip(times, temperatures, precipitations, wind_speeds):
             cursor.execute(query, (loc_id, t, temp, precip, wind))
-            
+
         connect.commit()
         print(f"Successfully loaded 5 years of data for {zone_name}!")
-        
+
     cursor.close()
     connect.close()
     print("All geographic zones have been fully ingested into MySQL.")
-        
